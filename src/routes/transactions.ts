5212e2f82ac260fa5982d61033a21426b4aaaa6d
OTAIMER20 <otaimer@outlook.com>
2025-08-06 17:56:06 +0200

    new routes and types knex config

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,48 @@
 import { FastifyInstance } from 'fastify'
 import { db } from '../database'
+import { z } from 'zod'
+import { randomUUID } from 'crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = await db('transactions').select('*')
+  app.get('/', async () => {
+    const transactions = await db('transactions').select()
 
-    return transactions
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+
+    const transaction = await db('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
+
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await db('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
